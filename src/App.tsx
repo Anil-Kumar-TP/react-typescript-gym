df738533e24bd3fa78be3e3f369cc78eb648b14b
Anil <anil6996f@gmail.com>
2024-07-02 21:28:48 +0530

    Working on Navbar for Desktop screens. Also implemented enums for the Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import Navbar from "@/scenes/navbar"
+import { useState } from "react"
+import { SelectedPage } from "@/shared/types";
+
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
   
   return (
-    <div className="text-green-600 text-7xl">
-      app
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
-}
+  );
+};
 
 export default App
